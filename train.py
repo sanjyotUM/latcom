44d8f9daa8be98d4d4d43c64f705e341193b80f6
bimi-lab <bimilab.dearborn@gmail.com>
2020-12-21 00:01:40 -0500

    Training script works

--- train.py
+++ train.py
@@ -1,97 +1,93 @@
-from pathlib import Path
 import argparse
+import cPickle as pickle
+import json
+from pathlib import Path
+from datetime import datetime, timedelta
+import random
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import tensorflow as tf
-from data import CommaData
+tf.get_logger().setLevel('ERROR')
+
+from preprocess import preprocess_image
+from data import CE491data
 from models import load_nvidia
-from utils import get_episode_name, save_indices, save_history
 
 parser = argparse.ArgumentParser(description='Train model and record performance.')
-
-# Data params
-parser.add_argument('--h5file', help='H5 file name', default='2016-01-30--11-24-51.h5', type=str)
-parser.add_argument('--timeshift', help='Number of milliseconds to shift labels in future', default=None, type=int)
-parser.add_argument('--start-index', help='Index to start reading data from', default=0, type=int)
-parser.add_argument('--end-index', help='Index to start reading data from', type=int)
-parser.add_argument('--batch-size', help='Training batch size', default=128, type=int)
-
-# Model params
-parser.add_argument('--learning-rate', help='Training learning rate', default=1e-4, type=float)
-
-# Training params
-parser.add_argument('--epochs', help='Training epochs', default=5, type=int)
-parser.add_argument('--workers', help='Number of threads creating batches', default=10, type=int)
-parser.add_argument('--queue-size', help='Maximum queue size of batches to create', default=20, type=int)
-parser.add_argument('--cpu-threads', help='If using cpu, how many threads to use', default=None, type=int)
+parser.add_argument('--epochs', help='Number of epochs to run training', default=3, type=int)
+parser.add_argument('--batch_size', help='Number of records in a mini batch', default=64, type=int)
+parser.add_argument('--time_shift', help='Number of milliseconds to shift labels in future', default=0, type=int)
+parser.add_argument('--lr', help='Learning rate', default=1e-4, type=float)
+parser.add_argument('--height', help='Input image height in pixels', default=40, type=int)
+parser.add_argument('--width', help='Input image width in pixels', default=80, type=int)
 
 args = parser.parse_args()
 
-if args.timeshift:
-    args.timeshift = args.timeshift // 10  # Convert milliseconds to steps
-
-if args.cpu_threads:
-    tf.config.threading.set_intra_op_parallelism_threads(args.cpu_threads)
-    tf.config.threading.set_inter_op_parallelism_threads(args.cpu_threads)
-
 project_dir = Path.cwd()
-lbl_dir = project_dir / 'dataset' / 'raw' / 'log'
-img_dir = project_dir / 'dataset' / 'raw' / 'camera'
-img_path = img_dir / args.h5file
-lbl_path = lbl_dir / args.h5file
-
+data_dir = project_dir / 'dataset'
 model_dir = project_dir / 'models'
-result_dir = project_dir / 'results'
-model_dir.mkdir(exist_ok=True)
-result_dir.mkdir(exist_ok=True)
+if not model_dir.exists():
+    model_dir.mkdir()
 
-episode_name = get_episode_name(args)
-model_name = episode_name + '.h5'
-model_path = model_dir / model_name
-episode_dir = result_dir / episode_name
-episode_dir.mkdir(exist_ok=True)
-
-filepaths = {
-    'lbl_path': lbl_path,
-    'img_path': img_path
+summary_filename = 'summary.csv'
+split_info = {
+    'train': [
+        'yild5350/2019-12-13-14-54-15',
+        'yild5350/2019-12-14-17-01-33',
+        # 'Jaku6779/2019-12-07-18-14-00',
+    ],
+    'valid': [
+        'Jaku6779/2019-12-16-19-31-06',
+        'yild5350/2019-12-15-17-13-05',
+        # 'jack4815/2019-12-15-21-09-25',
+    ]
 }
 
-comma = CommaData(
-    filepaths=filepaths,
-    label_shift_steps=args.timeshift,
-    start_index=args.start_index,
-    end_index=args.end_index,
-    img_shape=(160, 320, 3),
-    validation_split=0.15,
-    batch_size=args.batch_size
+ce = CE491data(
+    data_dir=data_dir,
+    summary_filename=summary_filename,
+    time_shift=args.time_shift,
+    batch_size=args.batch_size,
+    split_info=split_info,
+    cache=True
 )
+train_gen, valid_gen, t, v = ce.get_train_valid_generators()
 
-train, valid, train_index, valid_index = comma.get_train_valid_datasets()
-save_indices(train_index, valid_index, episode_dir)
+nvidia = load_nvidia(input_shape=(args.height, args.width, 3), learning_rate=args.lr)
+nvidia.summary()
 
-nvidia = load_nvidia(
-    learning_rate=args.learning_rate,
-    input_shape=(160, 320, 3)
-)
-
-log_dir = project_dir / 'logs' / episode_name
-log_dir.mkdir(parents=True, exist_ok=True)
-tensorboard_callback = tf.keras.callbacks.TensorBoard(
-    log_dir=str(log_dir),
-    histogram_freq=1,
-    update_freq='batch',
-    write_images=True,
-)
-print(f'\nLog dir: {log_dir}\n')
-
-history = nvidia.fit(
-    train,
+history = nvidia.fit_generator(
+    train_gen,
     epochs=args.epochs,
-    validation_data=valid,
-    workers=args.workers,
-    max_queue_size=args.queue_size,
-    callbacks=[tensorboard_callback]
+    steps_per_epoch=np.ceil(len(t)/float(args.batch_size)),
+    verbose=1,
+    validation_data=valid_gen,
+    max_queue_size=20,
+    workers=8,
+    validation_steps=np.ceil(len(v)/float(args.batch_size)),
+    # callbacks=[tensorboard_callback],
+    use_multiprocessing=True
 )
 
-nvidia.save(str(model_path))
-print(f'Model saved at: {model_path}')
-save_history(history.history, episode_dir)
+model_name = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+results_dir = project_dir / 'models' / model_name
+results_dir.mkdir()
+weight_path = results_dir / 'weights.h5'
+model_config_path = results_dir / 'config.json'
+history_path = results_dir / 'history.pkl'
+params_path = results_dir / 'params.json'
+
+# Save all files
+nvidia.save_weights(weight_path)
+json_config = nvidia.to_json()
+with open(str(model_config_path), 'w') as json_file:
+    json_file.write(json_config)
+with open(str(history_path), 'wb') as f:
+    pickle.dump(history, f)
+with open(str(params_path), 'w') as json_file:
+    json.dump(vars(args), json_file)
+
+print('Model saved at: {}'.format(results_dir))
