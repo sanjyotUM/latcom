e7df5166e5fd9f7f798a1cd4c773208643cca3f7
bimi-lab <bimilab.dearborn@gmail.com>
2020-12-22 10:37:10 -0500

    Set ce491 path

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 # oscar main folder location (change the path if required)
 bimi_user_dir=$(cd ../..; pwd)
-export OSCAR_PATH="${bimi_user_dir}/jaerock/oscar"
+export CE491_PATH="${bimi_user_dir}/working/ce491-2019/final_project"
